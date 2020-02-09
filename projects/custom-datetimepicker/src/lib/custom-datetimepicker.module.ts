7d0db366f2995de3200fc254524efd8618e96c01
Saurabh Kulkarni <saurabhk369@gmail.com>
2020-02-09 18:53:42 +0530

    Bug fixes. Code moved from component to library

--- projects/custom-datetimepicker/src/lib/custom-datetimepicker.module.ts
+++ projects/custom-datetimepicker/src/lib/custom-datetimepicker.module.ts
@@ -1,9 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CustomDatetimepickerComponent } from './custom-datetimepicker.component';
 
 @NgModule({
   declarations: [CustomDatetimepickerComponent],
   imports: [
+    BrowserModule,
+    FormsModule
   ],
   exports: [CustomDatetimepickerComponent]
 })
